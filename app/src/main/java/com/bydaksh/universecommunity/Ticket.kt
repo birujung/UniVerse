6d6b6d2b1372d329d115ceefe71b8f1be02ee327
farrelmrwn <muhammad.farrel13@ui.ac.id>
2023-12-11 00:13:26 +0700

    Harusnya udah bisa cuman firebase bingung

---
+++ app/src/main/java/com/bydaksh/universecommunity/Ticket.kt
@@ -0,0 +1,14 @@
+package com.bydaksh.universecommunity
+
+class  Ticket{
+    var tweetID:String?=null
+    var tweetText:String?=null
+    var tweetImageURL:String?=null
+    var tweetPersonUID:String?=null
+    constructor(tweetID:String,tweetText:String,tweetImageURL:String,tweetPersonUID:String){
+        this.tweetID=tweetID
+        this.tweetText=tweetText
+        this.tweetImageURL=tweetImageURL
+        this.tweetPersonUID=tweetPersonUID
+    }
+}
